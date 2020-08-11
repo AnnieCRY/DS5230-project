c9668ec3d6131ca7f53f8b23c95863c2c2a086c6
Tony Z 4516 <44876222+Tonyz4516@users.noreply.github.com>
2020-08-10 21:28:40 -0400

    Add files via upload

---
+++ relabel.py
@@ -0,0 +1,18 @@
+def modify_labels():
+    csv_file = "/content/processed_data/labels_BeeNotBee_th0.csv"
+    labels_BeeNotBee_th0 = pd.read_csv(csv_file)
+
+    def add_new_labels(df):
+        labels_l=[]
+        for x in df['label_strength']:
+            if x>0.95:
+                labels_l.append('nobee') 
+            elif x==0:
+                labels_l.append('bee') 
+            else:
+                labels_l.append('unknown') 
+        df['label']=labels_l
+        return df
+    
+    labels_BeeNotBee_th0=add_new_labels(labels_BeeNotBee_th0)
+    labels_BeeNotBee_th0.to_csv(csv_file)
