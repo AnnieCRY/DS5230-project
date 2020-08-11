ace9a987a9fd53930c91c742f9ff43f2b919a0ef
Tony Z 4516 <44876222+Tonyz4516@users.noreply.github.com>
2020-08-11 13:21:49 -0400

    Add files via upload

--- relabel.py
+++ relabel.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def modify_labels(path):
     csv_file = path + "/labels_BeeNotBee_th0.csv"
-    labels_BeeNotBee_th0 = pd.read_csv(csv_file)
+    labels_BeeNotBee_th0 = pd.read_csv(csv_file, index_col=0)
 
     def add_new_labels(df):
         labels_l=[]
@@ -17,4 +17,4 @@ def modify_labels(path):
         return df
     
     labels_BeeNotBee_th0=add_new_labels(labels_BeeNotBee_th0)
-    labels_BeeNotBee_th0.to_csv(csv_file)
+    labels_BeeNotBee_th0.to_csv(csv_file, index=False)
